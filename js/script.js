6765e1e9c0ad27599a77483f6fe305c2fffc1c33
Natan Lira <91631690+deliras23@users.noreply.github.com>
2022-05-01 13:21:34 -0300

    a

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function ing() {
+    //entrada
+    let ingresso = document.form.ingresso.value
+
+    //processamento
+    if (ingresso == espectador) {
+        valor = 80
+    }
+    
+    
+
+    //saída
+    document.getElementById("total").value = valor
+} 
